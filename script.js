bbb9fe8fe46f31c3408cd5d965f701a190cbb69e
Nkululeko Mthethwa <77917405+nsmthethwa44@users.noreply.github.com>
2022-02-17 19:08:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let menu = document.querySelector(".menu-img");
+let navigation = document.querySelector(".nav");
+
+menu.onclick = () =>{
+  menu.classList.toggle("close-img");
+  navigation.classList.toggle("active");
+}
+
+window.onscroll = () =>{
+  menu.classList.remove("close-img");
+  navigation.classList.remove("active");
+}
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+} 
+window.addEventListener("scroll", reveal);
